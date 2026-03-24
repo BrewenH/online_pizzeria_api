8a73c9da0fc3e8d4a7d6541fc2227bf0ea0a2767
murciana.le.bouedec <murciana.lb@gmail.com>
2026-03-24 13:42:50 +0100

    fonctionnalité add ingredient en TDD fait

--- src/main/java/com/accenture/onlinepizzeriaapi/controller/impl/IngredientController.java
+++ src/main/java/com/accenture/onlinepizzeriaapi/controller/impl/IngredientController.java
@@ -1,36 +1,62 @@
 package com.accenture.onlinepizzeriaapi.controller.impl;
 
 import com.accenture.onlinepizzeriaapi.controller.IngredientApi;
+import com.accenture.onlinepizzeriaapi.service.IngredientService;
 import com.accenture.onlinepizzeriaapi.service.dto.IngredientRequestDto;
 import com.accenture.onlinepizzeriaapi.service.dto.IngredientResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
+@RestController
+@AllArgsConstructor
 public class IngredientController implements IngredientApi {
+
+    private final IngredientService ingredientService;
+
     @Override
     public ResponseEntity<List<IngredientResponseDto>> findAll() {
-        return null;
+
+        return ResponseEntity.ok(ingredientService.findAll());
     }
 
     @Override
     public ResponseEntity<IngredientResponseDto> findById(UUID idIngredient) {
-        return null;
+
+        return ResponseEntity.ok(ingredientService.findById());
     }
 
     @Override
     public ResponseEntity<Void> addIngredient(IngredientRequestDto requestDto) {
-        return null;
+        log.info("Access to endpoint /POST/ingredients");
+        IngredientResponseDto responseDto = ingredientService.addIngredient(requestDto);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDto.id())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
     @Override
     public ResponseEntity<Void> deleteIngredient(UUID idIngredient) {
-        return null;
+        ingredientService.deleteIngredient(idIngredient);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
     @Override
     public ResponseEntity<IngredientResponseDto> patchIngredient(UUID idIngredient, IngredientRequestDto requestDto) {
-        return null;
+        IngredientResponseDto responseDto = ingredientService.patchIngredient(idIngredient,requestDto);
+        return ResponseEntity.ok(responseDto);
     }
 }
--- src/main/java/com/accenture/onlinepizzeriaapi/service/dto/IngredientRequestDto.java
+++ src/main/java/com/accenture/onlinepizzeriaapi/service/dto/IngredientRequestDto.java
@@ -1,4 +1,15 @@
 package com.accenture.onlinepizzeriaapi.service.dto;
 
-public record IngredientRequestDto(String name, int quantity) {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record IngredientRequestDto(
+
+        @NotBlank(message = "ingredient.name.null")
+        String name,
+
+        @NotNull(message = "ingredient.quantity.null")
+        @Min(value = 0, message = "ingredient.quantity.invalid")
+        int quantity) {
 }
